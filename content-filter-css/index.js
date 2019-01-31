d27182b30070c436382f7fa0db8ae8798a8a7d85
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-01-31 10:15:42 -0800

    content filter css

---
+++ content-filter-css/index.js
@@ -0,0 +1,3 @@
+/* PURE CSS FILTER
+for a simpler effect with fewer lines of CSS, trade the width, height, padding, margin, and opacity properties in each checked input style for "display:none;" -- these styles were just set to have a nice tranisition between filters
+*/
